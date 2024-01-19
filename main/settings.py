86d15400490d5c72e605f1b1abf3cfdc66173b35
Darren Robinson <darrenandamanda.robinson@gmail.com>
2024-01-19 17:20:57 +1100

    Update allowed host

--- main/settings.py
+++ main/settings.py
@@ -25,7 +25,7 @@ SECRET_KEY = read_file("secret_key")
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['54.79.27.18',]
 
 
 # Application definition
