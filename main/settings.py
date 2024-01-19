963cf03a3a3b4f3e26e8fd7c5fdd49d4f9b791f0
Darren Robinson <darrenandamanda.robinson@gmail.com>
2024-01-19 16:14:35 +1100

    Add static files into settings

--- main/settings.py
+++ main/settings.py
@@ -1,4 +1,5 @@
 from pathlib import Path
+import os
 
 def read_file(text):
     file = open("SECRET_KEYS.txt")
@@ -119,6 +120,14 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/4.2/howto/static-files/
 
 STATIC_URL = 'static/'
+STATIC_ROOT = os.path.join(BASE_DIR, 'static')
+STATICFILES_DIRS = [
+    os.path.join(BASE_DIR, 'app/static'),
+    # os.path.join(BASE_DIR, 'ckeditor'),
+    # os.path.join(BASE_DIR, 'ckeditor_uploader'),
+
+    # BASE_DIR / 'static',
+]
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/4.2/ref/settings/#default-auto-field
