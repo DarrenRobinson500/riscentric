c930ad949c02eb80e88535858743002950f1b83b
Darren Robinson <darrenandamanda.robinson@gmail.com>
2024-02-19 20:50:28 +1100

    Prep for Vercel

--- main/settings.py
+++ main/settings.py
@@ -128,15 +128,23 @@ USE_TZ = True
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/4.2/howto/static-files/
 
-STATIC_URL = 'static/'
-STATIC_ROOT = os.path.join(BASE_DIR, 'static')
-STATICFILES_DIRS = [
-    os.path.join(BASE_DIR, 'app/static'),
-    # os.path.join(BASE_DIR, 'ckeditor'),
-    # os.path.join(BASE_DIR, 'ckeditor_uploader'),
+# STATIC_URL = 'static/'
+# STATIC_ROOT = os.path.join(BASE_DIR, 'static')
+# STATICFILES_DIRS = [
+#     os.path.join(BASE_DIR, 'app/static'),
+#     # os.path.join(BASE_DIR, 'ckeditor'),
+#     # os.path.join(BASE_DIR, 'ckeditor_uploader'),
+#
+#     # BASE_DIR / 'static',
+# ]
 
-    # BASE_DIR / 'static',
-]
+STATIC_URL = 'static/'
+STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles_build', 'static')
+MEDIA_URLS ='/media/'
+MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
+
+
+
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/4.2/ref/settings/#default-auto-field
