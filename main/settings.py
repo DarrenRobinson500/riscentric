f9545b87962c5b02909be2029d381c97896cee54
Darren Robinson <darrenandamanda.robinson@gmail.com>
2024-02-20 23:00:09 +1100

    Update settings for media files

--- main/settings.py
+++ main/settings.py
@@ -127,18 +127,18 @@ USE_TZ = True
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/4.2/howto/static-files/
 
+# STATIC_URL = 'static/'
+# STATIC_ROOT = os.path.join(BASE_DIR, 'static')
+# STATICFILES_DIRS = [
+#     os.path.join(BASE_DIR, 'app/static'),
+#     # os.path.join(BASE_DIR, 'ckeditor'),
+#     # os.path.join(BASE_DIR, 'ckeditor_uploader'),
+#
+#     # BASE_DIR / 'static',
+# ]
+
 STATIC_URL = 'static/'
 STATIC_ROOT = os.path.join(BASE_DIR, 'static')
-STATICFILES_DIRS = [
-    os.path.join(BASE_DIR, 'app/static'),
-    # os.path.join(BASE_DIR, 'ckeditor'),
-    # os.path.join(BASE_DIR, 'ckeditor_uploader'),
-
-    # BASE_DIR / 'static',
-]
-
-# STATIC_URL = 'static/'
-# STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles_build', 'static')
 MEDIA_URLS ='/media/'
 MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
 
