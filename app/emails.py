e5c2e9d8020b764e588a29b945839209d37ac948
Darren Robinson <darrenandamanda.robinson@gmail.com>
2024-05-10 08:43:14 +1000

    Change DB

--- app/emails.py
+++ app/emails.py
@@ -5,14 +5,17 @@ def send_email_logic(ping):
     ping.sent = True
     ping.save()
 
+    # print("Email person questions:", ping.person_questions())
     for person_question in ping.person_questions():
-        if person_question.answer: continue
+        print("Email: ", person_question.answer)
+        if person_question.answer != "None" and person_question.answer != "Viewed": continue
         person = person_question.person
         question = person_question.question
         to = person_question.person.email_address
 
         email = Email(company=ping.company, ping=ping, person=person, question=question, person_question=person_question)
         email.save()
+        print("Created email:", email)
         send_microsoft_email(email, [to], send=True)
 
 
